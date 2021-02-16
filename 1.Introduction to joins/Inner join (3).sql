a8040e098cb66c19f325b4dc37f8cb001a428121
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-16 23:16:09 +0700

    Create Inner join (3).sql

---
+++ 1.Introduction to joins/Inner join (3).sql
@@ -0,0 +1,26 @@
+-- 4. Select fields
+SELECT c.code, c.name, c.region, p.year, p.fertility_rate
+  -- 1. From countries (alias as c)
+  FROM countries AS c 
+  -- 2. Join with populations (as p)
+  INNER JOIN populations AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code;
+    
+   -- 4. Join to economies (as e)
+  INNER JOIN economies AS e
+    -- 5. Match on country code
+    ON c.code = e.code;;
+    
+-- 6. Select fields
+SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
+  -- 1. From countries (alias as c)
+  FROM countries AS c
+  -- 2. Join to populations (as p)
+  INNER JOIN populations AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code
+  -- 4. Join to economies (as e)
+  INNER JOIN economies AS e
+    -- 5. Match on country code and year
+    ON c.code = e.code AND p.year = e.year;
