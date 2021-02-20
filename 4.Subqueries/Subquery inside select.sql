cbd324914126c8f2066613176969e2a027e1798e
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-20 21:34:22 +0700

    Create Subquery inside select.sql

---
+++ 4.Subqueries/Subquery inside select.sql
@@ -0,0 +1,36 @@
+SELECT countries.name AS country, COUNT(*) AS cities_num
+  FROM cities
+    INNER JOIN countries
+    ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
+/* 
+SELECT ___ AS ___,
+  (SELECT ___
+   FROM ___
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM ___
+ORDER BY ___ ___, ___
+LIMIT 9;
+*/
+-- Submit Answer
+
+
+SELECT countries.name AS country, COUNT(*) AS cities_num
+  FROM cities
+    INNER JOIN countries
+    ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
+
+SELECT countries.name AS country,
+  (SELECT COUNT(*)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9;
