eda01fec0ee9c129626106be3714fcb44851a62b
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-20 21:38:05 +0700

    Create Final challenge.sql

---
+++ 4.Subqueries/Final challenge.sql
@@ -0,0 +1,19 @@
+-- Select fields
+SELECT DISTINCT c.name, e.total_investment, e.imports
+  -- From table (with alias)
+  FROM countries AS c
+    -- Join with table (with alias)
+    LEFT JOIN economies AS e
+      -- Match on code
+      ON (c.code = e.code
+      -- and code in Subquery
+        AND c.code IN (
+          SELECT l.code
+          FROM languages AS l
+          WHERE official = 'true'
+        ) )
+  -- Where region and year are correct
+  WHERE year = 2015 AND region = 'Central America'
+-- Order by field
+ORDER BY c.name;
+
